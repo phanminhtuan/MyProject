b0c0d1db2de68236c8187ee5620e3989c5c0781d
phanminhtuan <thienlinhlong@gmail.com>
2014-11-01 16:24:39 +0700

    amyProject

---
+++ gen/tuan/matchlib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package tuan.matchlib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/tuan/matchlib/MatchLib.java
@@ -0,0 +1,9 @@
+package tuan.matchlib;
+
+public class MatchLib {
+//cong 2 so a,b
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
